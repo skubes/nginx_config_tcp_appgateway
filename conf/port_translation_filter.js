5cd68947e0d852665879e3132464a50e555d634a
root <root@rhtest.rxvxgbnxnm0ejhd4rtc4olwzfa.bx.internal.cloudapp.net>
2018-04-09 00:10:21 +0000

    check 10 packets

--- conf/port_translation_filter.js
+++ conf/port_translation_filter.js
@@ -4,7 +4,7 @@ function handler(s) {
     if (s.fromUpstream) { 
         upstreammessagecount++;
         //s.log("messagecount [" + upstreammessagecount + "]");
-        if (upstreammessagecount <= 9 ) {
+        if (upstreammessagecount <= 10 ) {
             s.buffer = s.buffer.replace(/\"Port\":37777/g, "\"Port\":80");
             s.buffer = s.buffer.replace(/^Port:37777/gm, "Port:80");
         }
