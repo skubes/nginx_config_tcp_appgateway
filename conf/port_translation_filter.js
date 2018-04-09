ee5f2fcbe7da956793134e6b8a729a4e7b770fe1
root <root@rhtest.rxvxgbnxnm0ejhd4rtc4olwzfa.bx.internal.cloudapp.net>
2018-04-09 13:32:35 +0000

    revert packet number tracking

--- conf/port_translation_filter.js
+++ conf/port_translation_filter.js
@@ -1,13 +1,7 @@
-var upstreammessagecount = 0;
-
 function handler(s) {
     if (s.fromUpstream) { 
-        upstreammessagecount++;
-        //s.log("messagecount [" + upstreammessagecount + "]");
-        if (upstreammessagecount <= 10 ) {
             s.buffer = s.buffer.replace(/\"Port\":37777/g, "\"Port\":80");
             s.buffer = s.buffer.replace(/^Port:37777/gm, "Port:80");
-        }
     }
     return;
 }
